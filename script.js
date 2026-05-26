ff06fb2cf8c20494c45f204d8423a05b0ac9d0c8
Shriya Keerthy <shriyakeerthy@gmail.com>
2026-05-25 20:21:58 -0500

    Makes image in image.html change to image clicked in gallery.html

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ function clickImage(this) {
     sessionStorage.setItem(currentImage, this.getAttribute.src);
 }
 
-function setImage(this) {
-    this.firstElementChild.src = sessionStorage.getItem(currentImage);
+function setPage() {
+    setImage();
+}
+
+function setImage() {
+    let img = document.getElementById("dependImage");
+    img.src = sessionStorage.getItem(currentImage);
 }
