fc03bcce5388534873b7dbba8f4a7162fbc5536b
Shriya Keerthy <shriyakeerthy@gmail.com>
2026-05-25 23:53:46 -0500

    Fix image changer

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ function clickImage(this) {
     sessionStorage.setItem(currentImage, this.getAttribute.src);
 }
 
-function setImage() {
-    let img = document.getElementById("dependImage");
-    img.src = sessionStorage.getItem(currentImage);
-}
+// function setImage() {
+//     let img = document.getElementById('dependImage').setAttribute('src', 'Images/Cat.png');
+//     // sessionStorage.getItem("");
+// }
+
+window.onload = function() {
+    document.getElementById('dependImage').src = 'Images/Cat.png';
+};
