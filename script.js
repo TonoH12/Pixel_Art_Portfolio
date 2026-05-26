0d54db93ed953b7f51f1aa6e8a98b5f8b9f4ac08
Shriya Keerthy <shriyakeerthy@gmail.com>
2026-05-25 20:26:50 -0500

    Fix image change in image.html

--- script.js
+++ script.js
@@ -2,10 +2,6 @@ function clickImage(this) {
     sessionStorage.setItem(currentImage, this.getAttribute.src);
 }
 
-function setPage() {
-    setImage();
-}
-
 function setImage() {
     let img = document.getElementById("dependImage");
     img.src = sessionStorage.getItem(currentImage);
