dcec72362febe0a2544c0d0e8ea9a7fcc79c5ff8
Shriya Keerthy <shriyakeerthy@gmail.com>
2026-05-29 00:09:29 -0500

    Reworked image changing to each image going to its own page
    The image changing in image.html still didn't fully work, and after an hour of trying to fix it, making individual page per image seemed to be a better and faster solution

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 function getImage(event) {
     event.preventDefault();
-    sessionStorage.setItem('givenImage', this.querySelector('img').src);
+    sessionStorage.setItem('givenImage', event.target.querySelector('img'));
     window.location.href = 'image.html';
 }
 
 function setPage() {
     // document.getElementById('dependImage').src = 'Images/Cat.png';
+    // sessionStorage.setItem('givenImage', 'Images/Cat.png');
     document.getElementById('dependImage').src = sessionStorage.getItem('givenImage');
+    // document.getElementById('checkerText').textContent = sessionStorage.getItem('givenImage');
 }
